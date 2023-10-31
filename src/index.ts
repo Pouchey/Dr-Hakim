57e126c90eb4b060b7bf3b36aac63a01d2f81d34
pouchey <theophile.rouchet@serli.com>
2023-10-31 12:10:14 +0100

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
 import * as dotenv from 'dotenv';
 import Client from './config/Client';
-import { InitDB } from './config/Db';
 
 dotenv.config();
 
 let client: Client;
-let db: any;
 
 const init = async () => {
   client = new Client();
-  db = await InitDB();
 
-  return { client, db };
+  return { client };
 };
 
 (async () => {
-  const { client, db } = await init();
+  const { client } = await init();
 
   client.start();
-
-  console.log(db);
 })();
 
 export const getClient = () => client;
-export const getDb = () => db;
