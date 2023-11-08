8b7222770937738c71609235ad575d0dd45009de
Sortique <thibaud.devemy@orange.fr>
2023-11-08 19:11:01 +0100

    random citations from Dr. Hakim Ferrier-Bellahouari

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,15 +1,28 @@
-import { Events, Message } from 'discord.js';
-import { getRandomReactionArray } from '../modules/quoi/utils';
+import {Events, Message} from 'discord.js';
+import {getRandomReactionArray, getRandomMessage} from '../modules/quoi/utils';
 
 export default {
-  name: Events.MessageCreate,
-  execute: async (message: Message) => {
-    if (message.content.toLowerCase().includes('quoi')) {
-      const reactionArray = getRandomReactionArray();
+    name: Events.MessageCreate,
+    execute: async (message: Message) => {
 
-      for (const reaction of reactionArray) {
-        await message.react(reaction);
-      }
+        // on vérifie que le message n'a pas été envoyé par le bot lui-même
+        if (message.author.id === message.client.user?.id) {
+            return;
+        }
+
+        if (message.content.toLowerCase().includes('quoi')) {
+            const reactionArray = getRandomReactionArray();
+
+            for (const reaction of reactionArray) {
+                await message.react(reaction);
+            }
+        } else {
+            const randomMessage = getRandomMessage();
+
+            if (randomMessage) {
+                await message.reply(randomMessage);
+            }
+        }
     }
-  }
 };
+
